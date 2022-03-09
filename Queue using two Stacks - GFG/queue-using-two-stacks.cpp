bef368d71752299fde80ea07c8325181b0d9ac3d
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-03-09 02:54:38 -0800

    Added solution - LeetHub

---
+++ Queue using two Stacks - GFG/queue-using-two-stacks.cpp
@@ -0,0 +1,87 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class StackQueue{
+private:
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int B);
+    int pop();
+
+};
+int main()
+{
+
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        StackQueue *sq = new StackQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+
+
+}
+// } Driver Code Ends
+
+
+/* The structure of the class is
+class StackQueue{
+private:   
+    // These are STL stacks ( http://goo.gl/LxlRZQ )
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int);
+    int pop();
+}; */
+
+//Function to push an element in queue by using 2 stacks.
+void StackQueue :: push(int x)
+{
+    // Your Code
+    s1.push(x);
+}
+
+//Function to pop an element from queue by using 2 stacks.
+int StackQueue :: pop() {
+        // Your Code   
+    int top = -1;
+    
+    if(s2.size()>0) {
+        
+        top = s2.top();
+        s2.pop();
+        
+    }else {
+        
+        while(s1.size()){
+            top = s1.top();
+            s2.push(top);
+            s1.pop();
+        }
+        if(s2.size()>0) s2.pop();
+            
+    }
+    return top;
+    
+}
