cfeb7b045cff1ea9358a0229d4fc549b13dff643
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-03-16 04:24:18 -0700

    Time: 15 ms (42.87%), Space: 15.3 MB (59.16%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         stack<int> s1;
-        queue<int> poppedQ;
-        for(auto a: popped) poppedQ.push(a);
+        int p = 0;
         for(auto a: pushed){
             s1.push(a);
-            while(!s1.empty() and s1.top()==poppedQ.front()){
+            while(!s1.empty() and s1.top()==popped[p]){
                 s1.pop();
-                poppedQ.pop();
+                p++;
             }
         }
         if(s1.empty()) return true;
-        return false;
-        
+        return false;  
     }
 };
