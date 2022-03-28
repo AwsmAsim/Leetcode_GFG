3dd7fec468924d1338e751de2003ad0841759a61
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-03-28 11:19:17 -0700

    Time: 10 ms (44.20%), Space: 14 MB (32.08%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int start = 0, end=nums.size()-1, mid;
+        while(nums[start] == nums[end] and start < end){
+            ++start;
+        }
+        while(start<=end){
+            mid = start + (end - start)/2;
+            // cout << mid << endl;
+            if(nums[mid] == target or start == end){
+                // cout << start << " " << end << endl;
+                break;
+            }
+            if(nums[mid] >= nums[start]){
+                // array is sorted on the left
+                if(target <= nums[mid] and target >= nums[start]){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }else if(nums[mid] <= nums[end]){
+                // array is sorted on right of mid
+                if(target >= nums[mid] and target <= nums[end]){
+                    start = mid + 1;
+                }else{
+                    end = mid - 1;
+                }
+            }
+        }
+        
+        if(target == nums[mid]) return true;
+        else return false;
+    }
+};
