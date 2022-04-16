a9a0cbeb0ffce975061a01e414aec3475ae47973
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-04-15 17:20:04 -0700

    Time: 19 ms (66.43%), Space: 23.9 MB (74.86%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -1,17 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+    
+    TreeNode* findValidRoot(TreeNode* node, int low, int high){
+        if(node==NULL) return NULL;
+        if(node->val >= low and node->val <= high) return node;
+        if(node->val < low) return findValidRoot(node->right, low, high);
+        if(node->val > high) return findValidRoot(node->left, low, high);
+        return node;
+    }
+    
+    void trim(TreeNode* node, int low, int high){
+        if(node == NULL) return;
+        //check Left
+        node->left = findValidRoot(node->left, low, high);
+        // if(node->left != NULL and node->left->val < low){
+        //     node->left = node->left->right;
+        // }
+        
+        node -> right = findValidRoot(node->right, low, high);
+        //check Right
+        // if(node->right != NULL and node->right->val > high){
+        //     node->right = node->right->left;
+        // }
+        
+        if(node->left != NULL) trim(node->left, low, high);
+        if(node->right != NULL) trim(node->right, low, high);
+        return;
+    }
+    
+    
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
-        if(!root) return nullptr;
-        
-        if(root->val < low) {
-            return trimBST(root->right, low, high);
-        } else if (root->val > high) {
-            return trimBST(root->left, low, high);
-        }
-        
-        root->left = trimBST(root->left, low, high);
-        root->right = trimBST(root->right, low, high);
-        
-        return root;
+        TreeNode* validNode = findValidRoot(root, low, high);
+        trim(validNode, low, high);
+        return validNode;
     }
 };
