3d413b4395c79402963baf1afd25e22e37230246
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-05-10 23:49:05 -0700

    Time: 170 ms (46.87%), Space: 85.5 MB (61.62%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
         int n = nums.size();
-        int sum1 = 0;
+        int sum1 = 0, lSum = 0;
         
         vector<int> prefSum(n, 0), answer(n,0);
         prefSum[0] = nums[0];
-        for(int i = 1; i < nums.size(); i++){
-            prefSum[i] = prefSum[i-1] + nums[i];
-        }
+        for(int i = 0; i < nums.size(); i++) sum1 += nums[i];
+        // cout << sum1<< endl;
         
         for(int i = 0; i < nums.size(); i++){
             int rightSum, leftSum;
             // cout << "for " << i << ":" << endl;
             if(i == 0) leftSum = 0;
-            else leftSum = prefSum[i-1] - (nums[i]*(i));
-            // cout << "rightSum: " << leftSum  << ", ";
+            else leftSum = lSum - (nums[i]*(i));
+            // cout << "leftSim: " << leftSum  << ", ";
             
             if(i==nums.size()-1) rightSum = 0;
-            else rightSum = prefSum[nums.size()-1] - prefSum[i] - (nums[i]*(n-i-1));
-            // cout << "leftSum: " << leftSum << endl;
+            
+            // prefSum[i] = lSum + nums[i], prefSum[nums.size()-1] = sum1
+            else rightSum = sum1 - (lSum + nums[i]) - (nums[i]*(n-i-1));
+            // cout << "rightSum: " << rightSum << endl;
             
             answer[i] = abs(rightSum) + abs(leftSum);
+            lSum += nums[i];
         }
         
         return answer;
