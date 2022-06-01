1e6dfd96b419898b0af6ca8475421cd89751de68
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-06-01 12:17:04 -0700

    Time: 6 ms (32.99%), Space: 8.6 MB (38.06%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        for(int i = 1; i < nums.size(); ++i){
+        for(int i = 1; i < nums.size(); i++){
             nums[i] += nums[i-1];
         }
         
