66c1fe7646e0440076bb9ee6a9e37808ce043c99
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-06-09 06:06:10 -0700

    Time: 2 ms (40.66%), Space: 6.2 MB (30.44%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    
+    int bottomUp(int n){
+        
+        if(n==1) return 1;
+        if(n==2) return 2;
+        
+        vector<int> dp(n+1, 0);
+        dp[1] = 1; 
+        dp[2] = 2;
+        
+        
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+        
+    }
+    
+public:
+    int climbStairs(int n) {
+        return bottomUp(n);
+    }
+};
