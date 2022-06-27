76ba571f8682bdb28e62b8db3f0ac705a9e093b5
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-06-26 23:05:10 -0700

    Time: 17 ms (93.22%), Space: 19.6 MB (58.46%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate = nums[0], count = 0;
+        int candidate = nums[0], vote = 0;
         for(int i = 0; i < nums.size(); i++){
-            cout << candidate << " ";
-            if(nums[i] == candidate){
-                count++;
-            }else{
-                count--;
-                if(count < 0){
+            if(nums[i]==candidate) ++vote;
+            else if(vote > 0){
+                --vote;
+                if(vote == 0)
+                {
                     candidate = nums[i];
-                    count = 0;
+                    ++vote;
                 }
-            }
+            }else candidate =nums[i];
+            
         }
         
         return candidate;
