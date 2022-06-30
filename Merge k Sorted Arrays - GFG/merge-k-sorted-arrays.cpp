a7283f620a2bffa2cadf48a2875576d356f87a68
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-06-30 03:10:37 -0700

    Added solution - LeetHub

--- Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.cpp
+++ Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.cpp
@@ -18,19 +18,55 @@ for (int i=0; i < size; i++)
 class Solution
 {
     public:
+    
+    vector<int> merge(vector<int> arr1, vector<int> arr2){
+        
+        vector<int> arr3(arr1.size() + arr2.size());
+        
+        int i = 0, j = 0, k = 0;
+        while(i < arr1.size() and j < arr2.size()){
+            if(arr1[i] < arr2[j]){
+                arr3[k++] = arr1[i++];
+            } else {
+                arr3[k++] = arr2[j++];
+            }
+        }
+        
+        while(i < arr1.size()){
+            arr3[k++] = arr1[i++];
+        }
+        
+        while(j < arr2.size()){
+            arr3[k++] = arr2[j++];
+        }
+        
+        return arr3;
+        
+    }
+    
     //Function to merge k sorted arrays.
     vector<int> mergeKArrays(vector<vector<int>> arr, int K)
     {
         //code here
         vector<int> ans;
         
-        for(int i=0; i < arr.size(); i++){
-            for(int j = 0; j < arr[i].size(); j++) ans.push_back(arr[i][j]);
+        for(int i = 0; i<arr.size(); i++){
+            ans = merge(ans, arr[i]);
         }
         
-        sort(ans.begin(), ans.end());
-        
         return ans;
+        
+        
+        // O(n*m) + O(n log n)
+        // vector<int> ans;
+        
+        // for(int i=0; i < arr.size(); i++){
+        //     for(int j = 0; j < arr[i].size(); j++) ans.push_back(arr[i][j]);
+        // }
+        
+        // sort(ans.begin(), ans.end());
+        
+        // return ans;
     }
 };
 
