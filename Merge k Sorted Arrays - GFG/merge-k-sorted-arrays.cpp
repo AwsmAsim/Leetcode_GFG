f1eddc4ad1d64e488db97c6868931f797fe95d2a
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-06-30 04:02:27 -0700

    Added solution - LeetHub

--- Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.cpp
+++ Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.cpp
@@ -19,54 +19,51 @@ class Solution
 {
     public:
     
-    vector<int> merge(vector<int> arr1, vector<int> arr2){
-        
-        vector<int> arr3(arr1.size() + arr2.size());
-        
-        int i = 0, j = 0, k = 0;
-        while(i < arr1.size() and j < arr2.size()){
-            if(arr1[i] < arr2[j]){
-                arr3[k++] = arr1[i++];
-            } else {
-                arr3[k++] = arr2[j++];
-            }
+    
+    
+struct comparator{
+        bool operator()(const pair<int,int> v1, const pair<int,int> v2){
+            return v1.first > v2.first;
         }
-        
-        while(i < arr1.size()){
-            arr3[k++] = arr1[i++];
-        }
-        
-        while(j < arr2.size()){
-            arr3[k++] = arr2[j++];
-        }
-        
-        return arr3;
-        
+    };
+    
+
+vector<int> sortWithHeap(vector<vector<int>> arr, int K){
+    // cout<<"1"<<" ";
+    
+    vector<int> ans;
+    
+    vector<int> itrs(arr.size(), 0);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, comparator> pq, pq2; // {value, outer array index}
+    
+    for(int i = 0; i < arr.size(); i++){
+        pq.push({arr[i][0], i});
     }
+    while(!pq.empty()){
+        // cout << 1 << " ";
+        pair<int, int> topPair = pq.top();
+        pq.pop();
+        itrs[topPair.second]++;
+        
+        if(itrs[topPair.second] < arr[topPair.second].size())
+            pq.push({arr[topPair.second][itrs[topPair.second]], topPair.second});
+        ans.push_back(topPair.first);
+        
+        
+        // print pq
+        // pq2 = pq;
+        // while(pq2)
+    }
+    
+    return ans;
+    
+}
     
     //Function to merge k sorted arrays.
     vector<int> mergeKArrays(vector<vector<int>> arr, int K)
     {
         //code here
-        vector<int> ans;
-        
-        for(int i = 0; i<arr.size(); i++){
-            ans = merge(ans, arr[i]);
-        }
-        
-        return ans;
-        
-        
-        // O(n*m) + O(n log n)
-        // vector<int> ans;
-        
-        // for(int i=0; i < arr.size(); i++){
-        //     for(int j = 0; j < arr[i].size(); j++) ans.push_back(arr[i][j]);
-        // }
-        
-        // sort(ans.begin(), ans.end());
-        
-        // return ans;
+        return sortWithHeap(arr, K);
     }
 };
 
