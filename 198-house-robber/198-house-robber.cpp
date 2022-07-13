22b4626762b3d2dfbede207a7fdf7965e1c5ff4b
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-07-13 11:09:49 -0700

    Time: 6 ms (16.73%), Space: 7.7 MB (75.68%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,6 +1,37 @@
 class Solution {
     
+    int recursive(vector<int>& nums, int idx)
+    {
+        if(idx >= nums.size()) return 0;
+        int pick = nums[idx] + recursive(nums, idx + 2);
+        int nonPick = recursive(nums, idx + 1);
+        
+        return max(pick, nonPick);
+    }
+    
+    int tab1(vector<int>& nums){
+        // cout << 1 << endl;
+        if(nums.size()==1) return nums[0];
+        if(nums.size()==2) return max(nums[0], nums[1]);
+
+        // cout << 2 << endl;
+        vector<int> dp(nums.size()+1, 0);
+        // cout << 3 << endl;
+        dp[1]=nums[0], dp[2] = max(nums[0], nums[1]);
+        
+
+        for(int i = 2; i <= nums.size(); i++){
+            dp[i] = max(nums[i-1] + dp[i-2], dp[i-1]);
+        }
+
+        // for(int a: dp) cout << a << ", ";
+        // cout << endl;
+        // return 0;
+        return dp[dp.size()-1];
+    }
+    
     int tabulation(vector<int>& nums){
+        
         if(nums.size()==1) return nums[0];
         if(nums.size()==2) return max(nums[0], nums[1]);
         
@@ -17,6 +48,8 @@ class Solution {
     
 public:
     int rob(vector<int>& nums) {
+        // return recursive(nums, 0);
+        return tab1(nums);
         return tabulation(nums);
     }
 };
