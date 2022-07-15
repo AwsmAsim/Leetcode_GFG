ba2df243d420477e13d3b95ceef508d2b505e034
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-07-14 19:13:02 -0700

    Time: 10 ms (15.31%), Space: 7.9 MB (12.57%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,5 +1,33 @@
 class Solution {
     
+    
+    int tabulation(vector<vector<int>>& grid){
+        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), 0));
+        
+        dp[0][0] = grid[0][0]  == 0 ? 1 : 0;
+        
+        for(int i = 0; i < dp.size(); i++){
+            for(int j = 0; j < dp[0].size(); j++){
+                if(i == 0 and j == 0) continue;
+                    if(grid[i][j] == 1) dp[i][j] = 0;
+                    else{
+                        // cout << "i: " << i << " j: " << j << endl;
+                        int up = i > 0 ? dp[i-1][j]:0, 
+                            left = j > 0 ? dp[i][j-1] : 0;
+                        // cout << "\tup:" << up << " left: " << left << endl;
+                        dp[i][j] = up + left;
+                    } 
+                
+            }
+        }
+        
+        // for(vector<int> row: dp){
+        //     for(int a : row) cout << a << ", ";
+        //     cout << endl;
+        // }
+        return dp.back().back();
+    }
+    
     int solveDP(vector<vector<int>>& grid){
         vector<vector<int>> ansGrid(grid.size(), vector<int>(grid[0].size(), 0));
         
@@ -28,6 +56,7 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         if(obstacleGrid[0][0]==1) return 0;
+        return tabulation(obstacleGrid);
         return solveDP(obstacleGrid);
     }
 };
