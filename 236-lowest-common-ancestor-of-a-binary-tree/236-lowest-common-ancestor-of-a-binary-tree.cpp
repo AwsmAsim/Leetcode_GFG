31f1b4caf0247ba4c5c2a501f239b35005bc8d87
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-07-20 21:12:21 +0530

    Time: 398 ms (5.09%), Space: 14.3 MB (25.30%) - LeetHub

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    int checkAvailablity(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root == NULL) return 0;
+        
+        if(root == p or root == q) return 1;
+        
+        if(checkAvailablity(root->left, p, q)) return 1;
+        else if(checkAvailablity(root->right, p, q)) return 1;
+        
+        return 0;
+        
+    }
+    
+    TreeNode* inorder(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root!= NULL){
+            TreeNode* lAnswer = inorder(root->left, p, q);
+            if(lAnswer != NULL ) return lAnswer;
+            
+            int avail = 0;
+            if(root == p or root == q){
+                avail = checkAvailablity(root->left, p, q) + checkAvailablity(root->right, p, q);
+                if(avail == 1) return root;
+            }else{
+                avail = checkAvailablity(root->left, p, q) + checkAvailablity(root->right, p, q);
+                if(avail == 2) return root;
+            }
+                
+            TreeNode* rAnswer = inorder(root->right, p, q);
+            if(rAnswer!=NULL) return rAnswer;
+        }
+        
+        return nullptr;
+    }
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return inorder(root, p, q);
+    }
+};
