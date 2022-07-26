88f385c1568ea6adb8ba273d5c8392b9c6fa27a8
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-07-26 11:55:15 +0530

    Time: 6 ms (56.76%), Space: 11 MB (75.00%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,37 +1,28 @@
 class Solution {
-    
-    
-    
-    
 public:
     int search(vector<int>& nums, int target) {
-        int start = 0, end=nums.size()-1, mid;
-        while(start<=end){
+        
+        int start = 0, end = nums.size()-1, mid;
+        
+        while(start <= end){
             mid = start + (end - start)/2;
-            // cout << mid << endl;
+            // cout << start << " " << end << " " << mid << endl;
             if(nums[mid] == target or start == end){
-                // cout << start << " " << end << endl;
                 break;
             }
+            // check if the left subarray is sorted
             if(nums[mid] >= nums[start]){
-                // array is sorted on the left
                 if(target <= nums[mid] and target >= nums[start]){
                     end = mid - 1;
-                }else{
+                }else start = mid + 1;
+            }else{
+                if(target <= nums[end] and target >= nums[mid]){
                     start = mid + 1;
-                }
-            }else if(nums[mid] <= nums[end]){
-                // array is sorted on right of mid
-                if(target >= nums[mid] and target <= nums[end]){
-                    start = mid + 1;
-                }else{
-                    end = mid - 1;
-                }
+                }else end = mid -1;
             }
         }
-        
-        if(target == nums[mid]) return mid;
-        else return -1;
+        if(nums[mid]!=target) return -1;
+        return mid;
         
     }
 };
