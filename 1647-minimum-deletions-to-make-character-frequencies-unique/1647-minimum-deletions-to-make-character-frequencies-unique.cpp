04a022bc03944c3320dd6429f6cbc1b79be93e97
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-08-13 01:36:21 +0530

    Time: 107 ms (37.46%), Space: 17.4 MB (51.46%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,58 +1,26 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        unordered_map<char, int> freqMap;
-        unordered_map<int, bool> availableMap;
-        vector<int> countList;
-        int del = 0;
         
-        for(char ch: s) ++freqMap[ch];
+        unordered_map<char, int> hMap;
+        for(char ch: s) ++hMap[ch];
         
-        for(auto val: freqMap){
-            availableMap[val.second] = true;
-            countList.push_back(val.second);
+        vector<int> frequencies;
+        for(pair<char, int> p: hMap){
+            frequencies.push_back(p.second);
         }
         
-        sort(countList.begin(), countList.end(), greater<int>());
+        sort(frequencies.begin(), frequencies.end(), greater<int>());
         
-//         // freq Map
-//         for(auto val: freqMap){
-//             cout << val.first << ": " << val.second << ",  ";
-//         }
-//         cout << endl;
-        
-//         // available Map
-//         for(auto val: availableMap) cout << val.first << ": " << val.second << ",  ";
-//         cout << endl;
-        
-//         // Count List
-//         cout << "{ ";
-//         for(auto c: countList) cout << c << ", ";
-//         cout << "} " << endl;
-        
-        
-        if(availableMap.size() == freqMap.size()) return 0;
-        
-        for(int i = 0; i < countList.size(); i++){
-            if(availableMap[countList[i]]) availableMap[countList[i]] = false;
-            else{
-                
-                // cout << "index: " << i  << ": " << endl;
-                while(availableMap.find(countList[i]) != availableMap.end()){
-                    --countList[i];
-                    ++del;
-                    // cout << "\tcountList[" << i << "]: " << countList[i] << " del: " << del << endl;
-                    if(countList[i] == 0) break;
-                    
-        //             for(auto val: availableMap) cout << val.first << ": " << val.second << ",  ";
-        // cout << endl;
-                }
-                availableMap[countList[i]] = false;
-            }
+        int t = INT_MAX, deletions = 0;
+
+        for(int i = 0; i < frequencies.size(); i++){
+            if(frequencies[i] >= t){
+                deletions += frequencies[i] - max(t-1, 0);
+                t = max(t-1, 0);
+            }else t = frequencies[i];
         }
         
-        return del;
-        
-        
+        return deletions;
     }
 };
