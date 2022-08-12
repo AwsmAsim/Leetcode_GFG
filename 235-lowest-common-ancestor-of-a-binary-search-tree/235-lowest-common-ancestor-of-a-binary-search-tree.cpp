45dd47b209945b0a8fbaf74bd5fa6a113f8bf02b
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-08-12 20:19:31 +0530

    Time: 39 ms (72.97%), Space: 23.3 MB (12.34%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,6 +10,25 @@
 
 class Solution {
     
+    
+    TreeNode* dailyChallenge1(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root == NULL) return NULL;
+        if(root == p) return p;
+        if(root == q) return q;
+        
+        if(p->val < root->val and q->val < root->val) return dailyChallenge(root->left, p ,q);
+        if(p->val > root->val and q->val > root->val) return dailyChallenge(root->right, p, q);
+        
+        TreeNode* left = dailyChallenge(root->left, p, q);
+        TreeNode* right = dailyChallenge(root->right, p, q);
+        
+        if(left == NULL) return right;
+        if(right == NULL) return left;
+        
+        return root;
+        
+    }
+    
     TreeNode* dailyChallenge(TreeNode* root, TreeNode* p, TreeNode* q){
         
         if(root == NULL) return NULL;
@@ -27,6 +46,6 @@ class Solution {
     
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return dailyChallenge(root, p, q);   
+        return dailyChallenge1(root, p, q);   
     }
 };
