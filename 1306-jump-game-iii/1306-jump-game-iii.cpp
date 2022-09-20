cb8588741817e692add6594b86603f4d92785c90
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-09-20 12:13:34 +0530

    Time: 112 ms (18.74%), Space: 31.6 MB (71.15%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -18,8 +18,31 @@ class Solution {
     }
     
     
+    bool bfs(int start, vector<int>& arr){
+        queue<int> Q;
+        Q.push(start);
+        
+        while(!Q.empty()){
+            int top = Q.front();
+            Q.pop();
+            
+            if(arr[top] < 0) continue;
+            if(arr[top] == 0) return true;
+            
+            if(arr[top]+top < arr.size())
+            Q.push(arr[top] + top);
+            if(top - arr[top] >= 0)
+            Q.push(top - arr[top] );
+            
+            arr[top] *= -1;
+        }
+        
+        return false;
+    }
+    
 public:
     bool canReach(vector<int>& arr, int start) {
+        return bfs(start, arr);
         return recursive(start, arr);
     }
 };
