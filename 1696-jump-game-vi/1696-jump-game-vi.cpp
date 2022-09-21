dc1398b786efe447f6ff2e20ee4ea3164c4f58b7
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-09-21 11:44:27 +0530

    Time: 533 ms (12.49%), Space: 93.9 MB (26.11%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,72 +1,59 @@
 class Solution {
+
     
-    struct comparator{
-        bool operator()(const pair<int, int>& p1, const pair<int, int>& p2){
-            return p1.first < p2.first;
-        }
-    };
-    
-    int recursive(vector<int>& nums, int& k, int idx){
+    int recursive(int idx, int& k, vector<int>& nums){
+        if(idx == nums.size()-1) return nums[nums.size()-1];
+        if(idx > nums.size()) return INT_MIN;
         
-        if(idx >= nums.size()) return -100001;
-        if(idx == nums.size()-1) return nums[idx];
         
-        int maxP = INT_MIN, points;
-        for(int i = 1; i <= k; ++i){
-            points = nums[idx] + recursive(nums, k, idx + i);
-            maxP = max(maxP, points);
+        int minn = INT_MIN;
+        for(int i = 1; i <= k; i++){
+            int recAns = recursive(idx + i, k, nums); 
+            if(recAns == INT_MIN) continue;
+            int ans = nums[idx] + recAns;
+            minn = max(minn, ans);
         }
         
+        return minn;
         
-        return maxP;
     }
     
-    int bottomUp(vector<int>& nums, int& k, int idx){
+    int tabulation(int& k, vector<int>& nums){
+        priority_queue<pair<int, int>> maxHeap;
+        // int prevMax = INT_MIN;
+        vector<int> dp(nums.size(), 0);
         
-        vector<int> dp(nums.size());
-        priority_queue<pair<int, int>, vector<pair<int, int>>, comparator> pq; // <int, int> = <value , idx>
-        
-        dp[0] = nums[0];
-        pq.push({dp[0], 0});
-        for(int i = 1; i < nums.size(); i++){
-            // if(i-k >= 0){
-                // int tMax = INT_MIN;
-                // for(int j = i - k; j < i; j++){
-                //     // cout << j <<" ";
-                //     tMax = max(dp[j], tMax);
-                // }
-                while(pq.top().second < i-k) pq.pop();
-                int tMax = pq.top().first;
-                // pq.pop();
-                // cout << endl;
-                // cout << "\t tMax: " << tMax << " " << nums[i] << " + " << tMax << endl;
-                dp[i] = nums[i] + tMax;
-                pq.push({dp[i], i});
-            // }
-        // else{
-        //         // int tMax = INT_MIN;
-        //         // for(int j = 0; j < i; j++){
-        //         //     // cout << j <<" ";
-        //         //     tMax = max(dp[j], tMax);
-        //         // }
-        //         int tMax = pq.top().first;
-        //         // pq.pop();
-        //         dp[i] = max(nums[i], nums[i]+tMax);   
-        //         pq.push({dp[i], i});
-        //     }
+        for(int i = 0; i < nums.size(); i++){
+            
+            pair<int, int> cMax;
+            int idx, val;
+            if(!maxHeap.empty()){
+                cMax = maxHeap.top();
+                idx = cMax.second, val = cMax.first;
+            }else{
+                val = 0;
+            }
+            
+            while(maxHeap.size() > k and idx < (i - k)){
+                maxHeap.pop();
+                cMax = maxHeap.top();
+                idx = cMax.second, val  = cMax.first;
+                // cout << idx << " " << endl;
+            }
+            
+            // cout << "val: " << val << endl;
+            dp[i] = nums[i] + val;
+            maxHeap.push({dp[i], i});
+            
         }
         
-        //print dp
-        // for(int value: dp) cout << value << ", ";
-        // cout << endl;
-        // if(dp[dp.size()-1] < 0) return 0;
-        return dp[nums.size()-1];
-        
+        return dp[dp.size()-1];
     }
+    
     
 public:
     int maxResult(vector<int>& nums, int k) {
-        return bottomUp(nums, k, 0);
-        return recursive(nums, k, 0);
+        return tabulation(k, nums);
+        return recursive(0, k, nums);
     }
 };
