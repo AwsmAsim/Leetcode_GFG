5cefd56c1584487697cc6a85088b901299a1d996
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-11-09 16:34:12 +0530

    Time: 554 ms (18.79%), Space: 84.3 MB (60.87%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -13,14 +13,41 @@ public:
         return count;
     }
     
+    
+    
     StockSpanner() {
         
     }
     
-    int next(int price) {
+    int nextNaive(int price) {
         int ans = findLeast(price);
         list.push_back(price);
         return ans;
+    }
+    
+    void _print(stack<pair<int, int>> s1){
+        
+        while(!s1.empty()){
+            int first = s1.top().first, second = s1.top().second;
+            cout << "(" << first  << ", " << second << "), ";
+            s1.pop();
+        }
+        cout << endl;
+    }
+    
+    stack<pair<int, int>> s1;
+    int next(int price) {
+        int count = 1;
+        while(!s1.empty() and s1.top().first <= price){
+            count += s1.top().second;
+            s1.pop();
+        }
+        s1.push(pair<int, int>(price, count));
+        
+        // // Print stack 
+        // _print(s1);
+        
+        return count;
     }
 };
 
